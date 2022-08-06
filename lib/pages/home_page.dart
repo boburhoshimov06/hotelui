17766276232704e72e1282fe3bba1dfc36026a10
boburhoshimov06 <boburhoshimov06@gmail.com>
2022-08-06 16:37:36 +0500

    hotelui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,28 +17,33 @@ class _HomePageState extends State<HomePage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
-              height: 300,
+              height: 250,
               decoration: BoxDecoration(
                 image: DecorationImage(
                     image: AssetImage('assets/images/ic_header.jpg'),
-                    fit: BoxFit.cover
-                ),
+                    fit: BoxFit.cover),
               ),
               child: Container(
                 decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        begin: Alignment.bottomRight,
-                        colors: [
-                          Colors.black.withOpacity(.8),
-                          Colors.black.withOpacity(.4),
-                        ]
-                    )
-                ),
+                    gradient:
+                        LinearGradient(begin: Alignment.bottomRight, colors: [
+                  Colors.black.withOpacity(.8),
+                  Colors.black.withOpacity(.4),
+                ])),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    Text("What kind of hotel you need?", textAlign: TextAlign.center, style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
-                    SizedBox(height: 30,),
+                    Text(
+                      "Best Hotels Ever",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
                     Container(
                       padding: EdgeInsets.symmetric(vertical: 3),
                       margin: EdgeInsets.symmetric(horizontal: 40),
@@ -50,56 +55,135 @@ class _HomePageState extends State<HomePage> {
                       child: TextField(
                         decoration: InputDecoration(
                             border: InputBorder.none,
-                            prefixIcon: Icon(Icons.search, color: Colors.grey,),
-                            hintStyle: TextStyle(color: Colors.grey, fontSize: 18),
-                            hintText: "Search for hotels..."
-                        ),
+                            prefixIcon: Icon(
+                              Icons.search,
+                              color: Colors.grey,
+                            ),
+                            hintStyle:
+                                TextStyle(color: Colors.grey, fontSize: 18),
+                            hintText: "Search for hotels..."),
                       ),
                     ),
-                    SizedBox(height: 30,)
+                    SizedBox(
+                      height: 30,
+                    )
                   ],
                 ),
               ),
             ),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 15,
+            ),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Best Hotels", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),),
-                  SizedBox(height: 20,),
+                  Text(
+                    "Business Hotels",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
+                        fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
                   Container(
                     height: 200,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        makeItem(image: 'assets/images/ic_hotel1.jpg', title: 'Hotel 1'),
-                        makeItem(image: 'assets/images/ic_hotel2.jpg', title: 'Hotel 2'),
-                        makeItem(image: 'assets/images/ic_hotel3.jpg', title: 'Hotel 3'),
-                        makeItem(image: 'assets/images/ic_hotel4.jpg', title: 'Hotel 4'),
-                        makeItem(image: 'assets/images/ic_hotel5.jpg', title: 'Hotel 5')
+                        makeItem(
+                            image: 'assets/images/ic_hotel1.jpg',
+                            title: 'Hotel 1'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel2.jpg',
+                            title: 'Hotel 2'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel3.jpg',
+                            title: 'Hotel 3'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel4.jpg',
+                            title: 'Hotel 4'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel5.jpg',
+                            title: 'Hotel 5')
                       ],
                     ),
                   ),
-                  SizedBox(height: 20,),
-
-                  Text("Luxury Hotels", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey[800], fontSize: 20),),
-                  SizedBox(height: 20,),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "Airport Hotels",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
+                        fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
                   Container(
                     height: 200,
                     child: ListView(
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        makeItem(image: 'assets/images/ic_hotel5.jpg', title: 'Hotel 1'),
-                        makeItem(image: 'assets/images/ic_hotel4.jpg', title: 'Hotel 2'),
-                        makeItem(image: 'assets/images/ic_hotel3.jpg', title: 'Hotel 3'),
-                        makeItem(image: 'assets/images/ic_hotel2.jpg', title: 'Hotel 4'),
-                        makeItem(image: 'assets/images/ic_hotel1.jpg', title: 'Hotel 5')
+                        makeItem(
+                            image: 'assets/images/ic_hotel5.jpg',
+                            title: 'Hotel 1'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel4.jpg',
+                            title: 'Hotel 2'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel3.jpg',
+                            title: 'Hotel 3'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel2.jpg',
+                            title: 'Hotel 4'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel1.jpg',
+                            title: 'Hotel 5')
                       ],
                     ),
                   ),
-                  SizedBox(height: 80,),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "Resort Hotels",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey[800],
+                        fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        makeItem(
+                            image: 'assets/images/ic_hotel3.jpg',
+                            title: 'Hotel 1'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel4.jpg',
+                            title: 'Hotel 2'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel5.jpg',
+                            title: 'Hotel 3'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel2.jpg',
+                            title: 'Hotel 4'),
+                        makeItem(
+                            image: 'assets/images/ic_hotel1.jpg',
+                            title: 'Hotel 5'),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             )
@@ -111,31 +195,27 @@ class _HomePageState extends State<HomePage> {
 
   Widget makeItem({image, title}) {
     return AspectRatio(
-      aspectRatio: 1.4 / 1,
+      aspectRatio: 1 / 1,
       child: Container(
         margin: EdgeInsets.only(right: 15),
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-                image: AssetImage(image),
-                fit: BoxFit.cover
-            )
-        ),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
         child: Container(
           padding: EdgeInsets.all(20),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(20),
-              gradient: LinearGradient(
-                  begin: Alignment.bottomRight,
-                  colors: [
-                    Colors.black.withOpacity(.8),
-                    Colors.black.withOpacity(.2),
-                  ]
-              )
-          ),
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+                Colors.black.withOpacity(.8),
+                Colors.black.withOpacity(.2),
+              ])),
           child: Align(
             alignment: Alignment.bottomLeft,
-            child: Text(title, style: TextStyle(color: Colors.white, fontSize: 20),),
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
           ),
         ),
       ),
